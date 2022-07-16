6a4ea9ab692051def1f30eb6123a6079ef8e70ad
nicknufayl <nikndaniel@gmail.com>
2022-07-16 17:06:23 +0800

    Done exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    var repeatedWord = ''
 
+    if (num < 0) {
+        return 'ERROR'
+    }
+
+    for (i = 0; i < num; i++) {
+        repeatedWord += word
+    }
+
+    return repeatedWord
 };
 
 // Do not edit below this line
