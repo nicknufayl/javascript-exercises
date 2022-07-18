116673379000dc42b50a977ceb710f97b15a5fd2
nicknufayl <nikndaniel@gmail.com>
2022-07-18 16:16:27 +0800

    Done exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    if (year%100 === 0)
+    {
+        if (year%400 === 0) return true
+        else return false
+    } else if (year%4 === 0) return true
+    else return false
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+
+if year%4 = 0 then leap year return true
+if year%100 > 0 then not leap year return false
+if year%400 = 0 then leap year return true
+
+*/
