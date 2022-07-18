20a61f9aac12dd6bb176ba8de18933b42cf33efb
nicknufayl <nikndaniel@gmail.com>
2022-07-18 16:32:07 +0800

    Done exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(value) {
+  temp = ((value - 32) * 5 / 9).toFixed(1);
+  return parseFloat(temp)
 };
 
-const ctof = function() {
-
+const ctof = function(value) {
+  temp = ((value * 9 / 5) + 32).toFixed(1);
+  return parseFloat(temp)
 };
 
 // Do not edit below this line
