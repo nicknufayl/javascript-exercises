1def9f0d0630a66e0ba6ce390b34b8d19ca7da82
nicknufayl <nikndaniel@gmail.com>
2023-04-08 16:29:54 +0800

    Done getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    const getBook = books.map(book => book.title) // the map() method is the hero here as it iterates through arrays 
+    return getBook
 };
 
 // Do not edit below this line
